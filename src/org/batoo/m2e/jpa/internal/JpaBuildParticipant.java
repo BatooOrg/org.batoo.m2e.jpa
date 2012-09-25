8571c324250751c090d40ed1cc1a22f4f30b06fa
hceylan <hceylan@ceylan.batoo.org>
2012-09-26 01:03:53 +0300

    Initial Commit

---
+++ src/org/batoo/m2e/jpa/internal/JpaBuildParticipant.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2012 - Batoo Software ve Consultancy Ltd.
+ * 
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.batoo.m2e.jpa.internal;
+
+import java.io.File;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.codehaus.plexus.util.Scanner;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.embedder.IMaven;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+public class JpaBuildParticipant extends MojoExecutionBuildParticipant {
+
+	public JpaBuildParticipant(MojoExecution execution) {
+		super(execution, true);
+	}
+
+	@Override
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
+		IMaven maven = MavenPlugin.getMaven();
+		BuildContext buildContext = getBuildContext();
+
+		// execute mojo
+		Set<IProject> result = super.build(kind, monitor);
+
+		// tell m2e builder to refresh generated files
+		File generated = maven.getMojoParameterValue(getSession(), getMojoExecution(), "outputDirectory", File.class);
+		if (generated != null) {
+			buildContext.refresh(generated);
+		}
+
+		return result;
+	}
+}
